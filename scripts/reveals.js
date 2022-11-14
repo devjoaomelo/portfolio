7cbdb00e28739b8d8024c8b28b809cfeef5feff6
joão <devjoaomelo@gmail.com>
2022-11-14 15:47:54 -0300

    pdt

--- scripts/reveals.js
+++ scripts/reveals.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', reveal);
-function reveal(){
+    function reveal(){
     var reveals = document.querySelectorAll('.reveal');
     for(var i = 0; i < reveals.length; i++){
         var windowheight = window.innerHeight;
@@ -7,8 +7,7 @@ function reveal(){
         var revealpoint = 150;
 
         if(revealtop < windowheight - revealpoint){
-            reveal[i].classList.add('active');
-
+            reveals[i].classList.add('active')
         }
         else {
             reveals[i].classList.remove('active');
